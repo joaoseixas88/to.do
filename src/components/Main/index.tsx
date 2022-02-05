07b38b11804fc283a8b68d17d8b7faace962b7fb
Joao Seixas <joaoseixas88@gmail.com>
2022-02-05 20:01:14 -0300

    final commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,33 +1,115 @@
 import './styles.scss'
-import { IoIosAddCircle } from "react-icons/io";
+import { FaPlusCircle } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import { useState } from 'react';
 
-type Task = string
+interface Task {
+    id: number;
+    title: string;
+    isComplete: boolean;
+}
+
+const tarefa: Task = {
+    id: 1,
+    title: 'Realizar alguma coisa',
+    isComplete: false
+}
+
+
+
 
 export function Main(){
 
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTaskTitle, setNewTaskTitle] = useState('')
+
+    function handleNewTask(){
+        if(!newTaskTitle){
+            return;
+        }
+        
+        const newTask = {
+            id: Math.random(),
+            title: newTaskTitle,
+            isComplete: false
+        }
+        setTasks([...tasks, newTask]);
+        setNewTaskTitle('')
+    }
+
+    function handleRemoveTask(id: number){
+        const removedTask = tasks.filter((task) => {
+            if (id !== task.id){
+                return ({
+                    id: task.id,
+                    title: task.title,
+                    isComplete: task.isComplete,
+                })
+            }            
+        })
+        setTasks(removedTask)
+    }
+
+    function handleCheckTask(id: number){
+        const newTasks = tasks.map((task) =>{
+            if(id === task.id){
+                return (
+                    {
+                        id: task.id,
+                        title: task.title,
+                        isComplete: !task.isComplete,
+                    }                  
+                ) 
+            } return task
+        })
+        setTasks(newTasks)     
+    }
 
     return(
-        <main>
+        <section>
             <div className='content'>
-                <div className='content-header'>
+                <header>
                     <h1>taks!</h1>
-
-                    <div className='div-input'>
-                        <input type="text" />
+                    <div className='div-input'>                    
+                        <input type="text"
+                        onChange={(event) => setNewTaskTitle(event.target.value)} 
+                        value={newTaskTitle}/>
+                        
                         <button className="add"
-                        type="submit"
+                        type="submit"    
+                        onClick={handleNewTask}
                         >
-                            <IoIosAddCircle />
+                           <FaPlusCircle color='#fff' size={16}/>
                         </button>
+                        
                     </div>
-                </div>
-                <div className='tasks'>
-                    <input type="checkbox" name="isComplete" id="1" />
-                    <span>Fazer o café</span>
-                </div>
+                </header>
+                <main className='tasks-container'>
+                    <ul>    
+                        {tasks.map((task) =>(
+                            <li key={task.id}>
+                                <div className={task.isComplete? 'completed' : ''} >
+                                    <label>
+                                        <input 
+                                        type="checkbox"
+                                        onClick={() => handleCheckTask(task.id)}
+                                        checked={task.isComplete}
+                                        />
+                                        <p>{task.title}</p>
+                                    </label>                                    
+                                </div>
+                                <div className='button-container'>
+                                <button
+                                onClick={() => handleRemoveTask(task.id)}
+                                >
+                                    <AiFillDelete />
+                                </button>                                
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </main>
             </div>
-        </main>
+        </section>
     )
 }
